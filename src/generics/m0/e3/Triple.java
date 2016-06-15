4f3664d8bc057ab395a794d9f8df796ba1e382e8
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-15 22:10:40 +0300

    Triple implemented according the task. Test passed

--- src/generics/m0/e3/Triple.java
+++ src/generics/m0/e3/Triple.java
@@ -1,6 +1,18 @@
 package generics.m0.e3;
 
-public class Triple {
+public class Triple<T1, T2, T3> extends Tuple<T1, T3> {
+
+    private final T2 middle;
+
+    public Triple(final T1 left, final T2 middle, final T3 right) {
+        super(left, right);
+        this.middle = middle;
+    }
+
+    public T2 getMiddle() {
+        return middle;
+    }
+
 }
 
 /*
